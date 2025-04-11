79099f055b8fef0e5407e4c523ce31526bba6d86
AllanClemenceau <clemenceau.all@gmail.com>
2025-04-11 16:03:08 +0200

    feat(config): add French news domains to Next.js image config
    - Add major French news domains to remotePatterns
    - Include Le Monde, Le Figaro, Liberation, BFMTV, and Radio France domains
    - Enhance image loading performance by using direct domain access

--- next.config.js
+++ next.config.js
@@ -14,6 +14,38 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'media1.giphy.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.dhnet.be',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.lsa-conso.fr',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.lemonde.fr',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.lemde.fr',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.lefigaro.fr',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.liberation.fr',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.bfmtv.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.radiofrance.fr',
+      },
     ],
   },
 }
